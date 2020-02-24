f4a46a7e6e4eb931a91d4ba9e783c9881f3d08c5
Tisdale Fry <tisdalefry@gmail.com>
2020-02-24 23:07:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,10 +16,8 @@ def hopper
         }
      }
 
-puts programmer_hash[:grace_hopper][:known_for]
-puts programmer_hash[:grace_hopper][:languages]
-
 end
+puts programmer_hash[:grace_hopper]
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
